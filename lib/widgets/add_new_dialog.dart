4cc56ba4c04c6a39c6a8920c129a8e2227c73764
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-09-19 11:59:48 +0530

    minor changes in add new property dialog

--- lib/widgets/add_new_dialog.dart
+++ lib/widgets/add_new_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iblock/widgets/forms/text_input_field.dart';
 
 class AddNewDialog extends StatelessWidget {
   const AddNewDialog({Key? key}) : super(key: key);
@@ -6,8 +7,14 @@ class AddNewDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-title: const Text('Add New'),
-      content: const Text('Add new item'),
+      title: const Text('Add New Entry'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextInputField("Property Name", padding: const EdgeInsets.all(8),),
+          TextInputField("Property Value", padding: const EdgeInsets.all(8),),
+        ],
+      ),
       actions: [
         TextButton(
           onPressed: () {
