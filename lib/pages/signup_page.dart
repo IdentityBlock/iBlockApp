e4f47056d68b8c9f2da069e60666c4b524d3df49
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-09-22 22:50:39 +0530

    use bloc to outline the signup

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/signup/signup_bloc.dart';
 
 import '../widgets/forms/text_input_field.dart';
 import '../widgets/forms/button.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final SignupBloc _signupBloc = SignupBloc();
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _dobController = TextEditingController();
+  final TextEditingController _countryController = TextEditingController();
+  final TextEditingController _mobileNumberController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +45,42 @@ class SignUpPage extends StatelessWidget {
                     'Sign Up',
                     style: Theme.of(context).textTheme.titleLarge,
                   )),
-              TextInputField('Full Name'),
-              TextInputField('Email'),
-              TextInputField('Date of Birth'),
-              TextInputField('Country'),
-              TextInputField('Mobile Number'),
-              TextInputField('Gender'),
+              TextInputField('Full Name', controller: _nameController),
+              TextInputField('Email', controller: _emailController),
+              TextInputField('Date of Birth', controller: _dobController),
+              TextInputField('Country', controller: _countryController),
+              TextInputField('Mobile Number', controller: _mobileNumberController),
+              TextInputField('Gender', controller: _genderController),
               const SizedBox(height: 30,),
-              Button('Sign up', onPressed: (){
-                print('Sign up');
-                Navigator.popAndPushNamed(context, '/home');
-              }),
+
+                BlocProvider(
+                  create: (context) => _signupBloc,
+                  child: BlocListener<SignupBloc, SignupState>(
+                    listener: (context, state) {
+                      if (state is Submitted){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Submitting...'),
+                          ),
+                        );
+                      }
+                      else if (state is Success) {
+                        Navigator.popAndPushNamed(context, '/home');
+                      }
+                    },
+                    child: Button('Sign Up', onPressed: () {
+                      _signupBloc.add(SubmitSignupEvent(
+                        name: _nameController.text,
+                        email: _emailController.text,
+                        dob: _dobController.text,
+                        country: _countryController.text,
+                        phone: _mobileNumberController.text,
+                        gender: _genderController.text
+                      ));
+                    }),
+                  ),
+
+              ),
               const SizedBox(height: 20,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
