38b0bbba9b0ccf458fb328984ba8f5f98b152202
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-09-22 22:16:21 +0530

    included TextEditor controller for custom TextInputField widget

--- lib/widgets/forms/text_input_field.dart
+++ lib/widgets/forms/text_input_field.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
-class TextInputField extends StatelessWidget {
+class TextInputField extends StatefulWidget {
   final String label;
+  TextEditingController controller = TextEditingController();
   EdgeInsets padding ;
 
-  TextInputField(this.label, {Key? key, this.padding = const EdgeInsets.only(top: 8, bottom: 8, left: 30, right: 30)}) : super(key: key);
+  TextInputField(this.label, {
+    Key? key,
+    TextEditingController? controller,
+    this.padding = const EdgeInsets.only(top: 8, bottom: 8, left: 30, right: 30)
+  }) : super(key: key);
 
+  @override
+  State<TextInputField> createState() => _TextInputFieldState();
+}
+
+class _TextInputFieldState extends State<TextInputField> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 8, bottom: 8, left: 30, right: 30),
       child: TextFormField(
+        controller: widget.controller,
         decoration: InputDecoration(
-          labelText: label,
+          labelText: widget.label,
           labelStyle: const TextStyle(
             color: Colors.black,
             fontSize: 16,
