209b01e9969f854019edf22794c05751044d85e1
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-11-07 09:21:41 +0530

    redesigned the settings page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,49 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:iblock/services/secure_storage_service.dart';
+
+import '../../widgets/copyable_text.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height -100,
-          width: double.infinity,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 30,
-              ),
-              Expanded(
-                  flex: 5,
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: const [
-                          Text("Private key:"),
-                          Text("Ox54s1hk25555dsd48522222", )
-                        ],
-                      )
-                    ],
-                  )),
-              Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+    return FutureBuilder(
+      future: loadData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        else if (snapshot.hasError){
+          Navigator.popAndPushNamed(context, "/error", arguments: {"message": "Encountered an error"});
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        else{
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Settings'),
+            ),
+            body: SafeArea(
+                child: SingleChildScrollView(
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height -100,
+                    width: double.infinity,
+                    child: Column(
                       children: [
-                    const Text("App version : "),
-                    Text("v0.1-beta", style: TextStyle(color: Theme.of(context).primaryColor),)
-                  ]))
-            ],
-          ),
-        ),
-      )),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Expanded(
+                            flex: 5,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        const Text("Private Key:"),
+                                        CopyableText((snapshot.data! as Map)["private-key"] as String)
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        const Text("Contract Address:"),
+                                        CopyableText((snapshot.data! as Map)["contract-address"] as String)
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )),
+                        Expanded(
+                            flex: 1,
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text("App version : "),
+                                  Text("v0.1-beta", style: TextStyle(color: Theme.of(context).primaryColor),)
+                                ]))
+                      ],
+                    ),
+                  ),
+                )),
+          );
+        }
+      }
     );
+  }
+
+  Future<Map<String, String>> loadData() async{
+    var privateKey = await SecureStorageService.get("private-key");
+    var contractAddress = await SecureStorageService.get("contract-address");
+    return {
+      "private-key" : privateKey as String,
+      "contract-address": contractAddress as String
+    };
   }
 }
---
+++ lib/widgets/copyable_text.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:fluttertoast/fluttertoast.dart';
+
+class CopyableText extends StatelessWidget {
+  final String value;
+  const CopyableText(this.value, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.grey,
+        backgroundBlendMode: BlendMode.colorDodge
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            flex: 9,
+            child: Text(value,
+            overflow: TextOverflow.fade,),
+          ),
+          Expanded(
+            flex: 1,
+            child: IconButton(onPressed: (){
+              Clipboard.setData(ClipboardData(text: value));
+              Fluttertoast.showToast(
+                msg: "Text copied",
+                gravity: ToastGravity.BOTTOM,
+                toastLength: Toast.LENGTH_SHORT
+              );
+            }, icon: const Icon(Icons.copy)),
+          )
+        ],
+      ),
+    );
+  }
+}
