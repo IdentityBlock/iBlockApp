12a49c91617c35d2dc4a0441d9dec01b9ab3ec10
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-11-16 19:47:54 +0530

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -6,6 +6,6 @@ class Config{
   static const String wsUrl = "wss://goerli.infura.io/v3/8b9d3fdeaccc4be4aeea708c77476676";
   static const int chainId = 5;
 
-  static const String backendUrl = "https://iblock-sever.herokuapp.com/";
+  static const String backendUrl = "https://iblock-sever.herokuapp.com";
 
 }
