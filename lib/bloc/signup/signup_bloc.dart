430d079d07d00534a09940f5101f09285e7c0d58
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-11-17 15:40:20 +0530

    bugfixes

--- lib/bloc/signup/signup_bloc.dart
+++ lib/bloc/signup/signup_bloc.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
-import 'package:iblock/bloc/initialize/initialize_bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
