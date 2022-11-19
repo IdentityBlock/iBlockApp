58b5572fe458cc459e091ef39b8bb32baeda771c
Mohamed Ishad <ishad.19@cse.mrt.ac.lk>
2022-11-19 19:05:48 +0530

    fixed config changes

--- lib/config.dart
+++ lib/config.dart
@@ -2,20 +2,13 @@
 // class contain all config information
 
 class Config{
-  // static const String rpcUrl = "https://goerli.infura.io/v3/8b9d3fdeaccc4be4aeea708c77476676";
-  // static const String wsUrl = "wss://goerli.infura.io/v3/8b9d3fdeaccc4be4aeea708c77476676";
-  // static const int chainId = 5;
-  //
-  // static const String backendUrl = "https://iblock-sever.herokuapp.com";
-
-  static const String VERSION = 'v1.0.0';
-
-  // development config
-  static const String rpcUrl = "http://192.168.8.163:8545";
-  static const String wsUrl = "http://192.168.8.163:8545";
+  static const String rpcUrl = "https://goerli.infura.io/v3/8b9d3fdeaccc4be4aeea708c77476676";
+  static const String wsUrl = "wss://goerli.infura.io/v3/8b9d3fdeaccc4be4aeea708c77476676";
   static const int chainId = 5;
 
-  static const String backendUrl = "http://192.168.8.163:3000";
+  static const String backendUrl = "https://iblock-sever.herokuapp.com";
+
+  static const String VERSION = 'v1.0.0';
 
 
 }
